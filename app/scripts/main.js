0238d49c29e2ae0631d5e67eeda2345aa5b228db
Jason Madigan <jason.madigan@feedhenry.com>
2014-08-20 13:17:20 +0100

    Styling updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,40 +1,103 @@
 var initcalled = false;
-$(document).ready(function(){
+$(document).ready(function() {
   $('.spinner').spin();
-  $fh.on('fhinit', function(err, cloud){
+  $fh.on('fhinit', function(err, cloud) {
     initcalled = true;
-    if(err){
+    if (err) {
       showErrorPanel('FH SDK failed to init. Something is wrong...');
     } else {
       loadApp();
     }
   });
-  setTimeout(function(){
-    if(!initcalled){
+  setTimeout(function() {
+    if (!initcalled) {
       showErrorPanel('FH SDK failed to init in 5 seconds. You should find out why...');
     }
   }, 5000);
 });
 
-function showErrorPanel(text){
+function showErrorPanel(text) {
   var p = $('<div>').addClass('panel panel-danger').append($('<div>').addClass('panel-heading').text('Error')).append($('<div>').addClass('panel-body').text(text));
   $('.results').empty().append(p);
 }
 
-function loadApp(){
+function loadApp() {
   $fh.cloud({
     path: '/recordTest',
     method: 'get',
     contentType: 'application/json'
-  }, function(res){
+  }, function(res) {
     $('.spinner').remove();
     var resultsCollection = new TestResults(res);
-    var resultsView = new TestResultsView({
-      collection: resultsCollection,
-      el: $('.results')[0]
+    var el = $('.content')[0];
+
+    // Sidebar utils
+    var Sidebar = {
+      removeActive: function() {
+        $('.sidebar-nav li').removeClass('active');
+      },
+      makeActive: function(link) {
+        this.removeActive();
+        $('.sidebar-nav a[href=' + link + ']').find('li').addClass('active');
+      }
+    };
+
+    var Router = Backbone.Router.extend({
+      routes: {
+        "about": "about",
+        "help": "help",
+        "results": "results",
+        "charts": "charts",
+        '*path': 'default'
+      },
+
+      help: function() {
+        var view = new HelpView({
+          collection: resultsCollection,
+          el: el
+        });
+        view.render();
+        Sidebar.makeActive('#help');
+      },
+
+      about: function() {
+        var view = new AboutView({
+          collection: resultsCollection,
+          el: el
+        });
+        view.render();
+        Sidebar.makeActive('#about');
+      },
+
+      charts: function() {
+        var view = new TestChartsView({
+          collection: resultsCollection,
+          el: el
+        });
+        view.render();
+        Sidebar.makeActive('#charts');
+      },
+
+      results: function() {
+        var view = new TestResultsView({
+          collection: resultsCollection,
+          el: el
+        });
+        view.render();
+        Sidebar.makeActive('#results');
+      },
+
+      default: function() {
+        this.navigate("results", {
+          trigger: true
+        })
+      }
     });
-    resultsView.render();
-  }, function(err){
+
+    var router = new Router();
+    Backbone.history.start();
+    window.router = router;
+  }, function(err) {
     showErrorPanel('Failed to load test results. You should find out why. Error : ' + err);
   });
 }
--- app/scripts/views.js
+++ app/scripts/views.js
@@ -1,61 +1,125 @@
 var TestResultsView = Backbone.View.extend({
-  
-  render: function(){
+  template: "#results-template",
+
+  render: function() {
     var self = this;
     console.log(this.collection.toJSON());
+
+    var content = $(this.template).html();
+    $(this.el).html(content);
+
     var tableEl = self.$el.find('table');
-    tableEl.dataTable({
+    var table = tableEl.dataTable({
       data: self.collection.toJSON(),
       paging: true,
-      pageLength: 20,
-      columns:[
-        {
-          title: 'Device',
-          data: 'deviceInfo.platform'  
-        },
-        {
-          title: 'OS Version',
-          data: 'deviceInfo.version',
-        },
-        {
-          title: 'UUID',
-          data: 'deviceInfo.uuid'
-        },
-        {
-          title: 'Device Model',
-          data: 'deviceInfo.model'
-        },
-        {
-          title: 'Cordova Version',
-          data: 'deviceInfo.cordova',
-        },
-        {
-          title: 'Test Time',
-          data: function(row, type){
-            var testInfo = row.testInfo;
-            var started = _.findWhere(testInfo, {stage: 'runner_starting'});
-            if(started){
-              var ts = started.ts;
-              return moment(ts).fromNow();
-            } else {
-              return 'Unknown';
-            }
-          }
-        },
-        {
-          title: 'Test Results (Failed/Total)',
-          data: function(row, type, set, meta){
-            var testInfo = row.testInfo;
-            var completeInfo = _.findWhere(testInfo, {stage:'complete'});
-            if(completeInfo){
-              var result = completeInfo.data.failed_specs + '/' + completeInfo.data.total_specs;
-              return result;
-            } else {
-              return 'Not Completed';
-            }
+      pageLength: 10,
+      "sDom": '<"top">rt<"bottom"lp><"clear">',
+      columns: [{
+        title: 'Device',
+        data: 'deviceInfo.platform'
+      }, {
+        title: 'OS Version',
+        data: 'deviceInfo.version',
+      }, {
+        title: 'UUID',
+        data: 'deviceInfo.uuid'
+      }, {
+        title: 'Device Model',
+        data: 'deviceInfo.model'
+      }, {
+        title: 'Cordova Version',
+        data: 'deviceInfo.cordova',
+      }, {
+        title: 'Test Time',
+        data: function(row, type) {
+          var testInfo = row.testInfo;
+          var started = _.findWhere(testInfo, {
+            stage: 'runner_starting'
+          });
+          if (started) {
+            var ts = started.ts;
+            return moment(ts).fromNow();
+          } else {
+            return 'Unknown';
           }
         }
-      ]
+      }, {
+        title: 'Test Results (Failed/Total)',
+        data: function(row, type, set, meta) {
+          var testInfo = row.testInfo;
+          var completeInfo = _.findWhere(testInfo, {
+            stage: 'complete'
+          });
+          if (completeInfo) {
+            var result = completeInfo.data.failed_specs + '/' + completeInfo.data.total_specs;
+            return result;
+          } else {
+            return 'Not Completed';
+          }
+        }
+      }]
     });
+
+    // Custom search
+    $('input.search').on('keyup', function(e) {
+      var search = $(e.currentTarget).val();
+      table.fnFilter(search);
+    });
+  }
+});
+
+
+var TestChartsView = Backbone.View.extend({
+  template: "#charts-template",
+
+  render: function() {
+    var self = this;
+    var content = $(this.template).html();
+    $(this.el).html(content);
+
+    this.renderOS();
+    this.renderCordova();
+    this.renderDevice();
+  },
+
+  renderOS: function() {
+    var el = this.$el.find('#os');
+    var data = this.collection.byOS();
+    var ctx = el.get(0).getContext("2d");
+    var chart = new Chart(ctx).Doughnut(data, this.doughnutOptions);
+  },
+
+  renderCordova: function() {
+    var el = this.$el.find('#cordova_version');
+    var data = this.collection.byCordova();
+    var ctx = el.get(0).getContext("2d");
+    var chart = new Chart(ctx).Doughnut(data, this.doughnutOptions);
+  },
+
+  renderDevice: function() {
+    var el = this.$el.find('#model');
+    var data = this.collection.byDevice();
+    var ctx = el.get(0).getContext("2d");
+    var chart = new Chart(ctx).Doughnut(data, this.doughnutOptions);
+  }
+});
+
+var AboutView = Backbone.View.extend({
+  template: "#about-template",
+
+  render: function() {
+    var self = this;
+    var content = $(this.template).html();
+    $(this.el).html(content);
+  }
+});
+
+var HelpView = Backbone.View.extend({
+  template: "#help-template",
+
+  render: function() {
+    var self = this;
+    var content = $(this.template).html();
+    $(this.el).html(content);
   }
 });
